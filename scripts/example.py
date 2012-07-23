efc690fb13a89b5d12a497bf642b35d72e608c57
nils.meier <nils.meier@400e30b4-3047-0410-9b5d-373f348d38bd>
2012-07-23 01:27:55 +0000

    no inadvertent gear down
    git-svn-id: https://fscode.googlecode.com/svn/trunk@310 400e30b4-3047-0410-9b5d-373f348d38bd

--- scripts/example.py
+++ scripts/example.py
@@ -10,20 +10,28 @@
 #   Map Push Button into Button (Missile Uncage)
 #   Map Rotation into Axis (Missile Acquisition Sound Level)
 #
-import joysticks, phidgets
+import joysticks, phidgets, state
+
+vjoy = joysticks.get('vJoy Device')
 
 # encoder 1 for axis and button
 encoder1 = phidgets.get(82141)
-vjoy = joysticks.get('vJoy Device')
-vjoy.setAxis(0, phidgets.encoder2axis(encoder1, 2))
+SimRadarElevation = phidgets.encoder2axis(encoder1, 2)
+vjoy.setAxis(0, SimRadarElevation)
 vjoy.setButton(0, encoder1.getInputState(0))
 
 # encoder 2 for axis and button
 encoder2 = phidgets.get(82081)
-vjoy.setAxis(1, phidgets.encoder2axis(encoder2, 1))
-vjoy.setButton(1, encoder2.getInputState(0))
+SimStepMissileVolume = phidgets.encoder2axis(encoder2, 1)
+SimToggleMissileCage = encoder2.getInputState(0)
+vjoy.setAxis(1, SimStepMissileVolume)
+vjoy.setButton(1, SimToggleMissileCage)
 
 # saitek axis 3 into 2 buttons
 saitek = joysticks.get('Saitek Pro Flight Quadrant')
-vjoy.setButton(2, saitek.getAxis(2) < 0)
-vjoy.setButton(3, saitek.getAxis(2) > 0)
+gearUp = saitek.getAxis(2) < 0
+if gearUp:
+    state.set("gear-seen-up", True)
+gearDown = not gearUp and state.get("gear-seen-up")
+vjoy.setButton(2, gearUp)
+vjoy.setButton(3, gearDown)

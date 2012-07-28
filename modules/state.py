ae213f3c85d123793672165dd2083838e7f87a9c
nils.meier <nils.meier@400e30b4-3047-0410-9b5d-373f348d38bd>
2012-07-28 03:31:11 +0000

    add toggle with 3 state result
    git-svn-id: https://fscode.googlecode.com/svn/trunk@321 400e30b4-3047-0410-9b5d-373f348d38bd

--- modules/state.py
+++ modules/state.py
@@ -5,13 +5,25 @@ def get(key,default=None):
     except KeyError:
         return default
 
-def set(key,val):
+def set(key,val): #@ReservedAssignment
     try:
         old =  __dict[key]
     except KeyError:
         old = val
     __dict[key] = val
     return old
+
+'''
+        N T F
+      N N T N
+      F N T N
+      T N N F
+'''
+def toggle(key, change):
+    old = set(key, change)
+    if not old and change: return True
+    if old and not change: return False
+    return None
 
 def inc(key):
     val = get(key, 0)

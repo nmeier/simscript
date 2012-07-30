9c9f272b10e8f3b288cab96db105f8d92117f467
nils.meier <nils.meier@400e30b4-3047-0410-9b5d-373f348d38bd>
2012-07-30 16:13:03 +0000

    beautify example for toggles
    git-svn-id: https://fscode.googlecode.com/svn/trunk@326 400e30b4-3047-0410-9b5d-373f348d38bd

--- scripts/example.py
+++ scripts/example.py
@@ -39,14 +39,16 @@ vjoy.setButton(3, gearDown)
 # saitex 1 button for gear toggle
 if state.toggle("gear", saitek.getButton(100)):
     log.info("Toggling gear")
-    keyboard.click(keyboard.VK_CAPITAL)
+    keyboard.click(keyboard.VK_G) # press/release virtual key code 
     
 # saitek 1 button for PTT while pressed
 ptt = state.toggle("PTT", saitek.getButton(101)) 
+if ptt == None:
+    pass # ignore no change
 if ptt == True:
     log.info("PTT on")
-    keyboard.press(keyboard.VK_SHIFT)
+    keyboard.press(keyboard.VK_SHIFT) # press virtual key code
+    keyboard.press(keyboard.VK_T)
 if ptt == False:
     log.info("PTT off")
-    keyboard.release(keyboard.VK_SHIFT)
-    
+    keyboard.release("shift t") # release keys (1. T, 2. Shift) 

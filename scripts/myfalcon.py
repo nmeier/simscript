2715a04f980a00f1de56a8067dd4ac78c34e7c50
nils.meier <nils.meier@400e30b4-3047-0410-9b5d-373f348d38bd>
2012-08-06 14:15:30 +0000

    add speedbrake on axis
    git-svn-id: https://fscode.googlecode.com/svn/trunk@344 400e30b4-3047-0410-9b5d-373f348d38bd

--- scripts/myfalcon.py
+++ scripts/myfalcon.py
@@ -2,6 +2,7 @@
 # My setup consists of:
 #
 #  Saitek Thottle Quadrant
+#   Split 2nd Axis into 2 Buttons (Speedbrake Extend/Retract)
 #   Split 3rd Axis into 2 Buttons (Gear Up/Down)
 #  Phidget Encoder 1
 #   Map Push Button into Button
@@ -10,7 +11,7 @@
 #   Map Push Button into Button (Missile Uncage)
 #   Map Rotation into Axis (Missile Acquisition Sound Level)
 #
-import joysticks, phidgets, state
+import joysticks, phidgets, state, log
 
 vjoy = joysticks.get('vJoy Device')
 
@@ -27,12 +28,26 @@ SimToggleMissileCage = encoder2.getInputState(0)
 vjoy.setAxis(1, SimStepMissileVolume)
 vjoy.setButton(1, SimToggleMissileCage)
 
-# saitek axis 3 into 2 buttons
+# saitek axis 2 into 2 buttons
 saitek = joysticks.get('Saitek Pro Flight Quadrant')
-gearUp = saitek.getAxis(2) < 0
-if gearUp:
-    state.set("gear-seen-up", True)
-gearDown = not gearUp and state.get("gear-seen-up")
-vjoy.setButton(2, gearUp)
-vjoy.setButton(3, gearDown)
+speedbrake = saitek.getAxis(1)
+retract = speedbrake < -0.5
+extend = speedbrake >  0.5
+if state.set("sbe", extend) != extend and extend:
+    log.info("extending speed brakes")
+if state.set("sbr", retract) != retract and retract:
+    log.info("retracting speed brakes")
+vjoy.setButton(2, extend)
+vjoy.setButton(3, retract)
+
+
+# saitek axis 3 into 2 buttons
+handleDown = saitek.getAxis(2) > 0
+if handleDown: # let's not accidentially retract gear unless we've seen handle down first
+    state.set("gear-seen-down", True)
+handleUp = not handleDown and state.get("gear-seen-down")
+if state.set("gear", handleUp) != handleUp:
+    log.info("Gear handle %s", ("down" if handleDown else "up"))
+vjoy.setButton(4, handleUp)
+vjoy.setButton(5, handleDown)
 

cd02ace4a1a3ea39ad5be6db29c7903d06a7298e
nils.meier <nils.meier@400e30b4-3047-0410-9b5d-373f348d38bd>
2012-08-10 20:58:24 +0000

    contribs, icon, etc.  ready to go I think
    git-svn-id: https://fscode.googlecode.com/svn/trunk@361 400e30b4-3047-0410-9b5d-373f348d38bd

--- package.py
+++ package.py
@@ -1,8 +1,29 @@
-from distutils.core import setup
-import py2exe
+import distutils.core, os, py2exe
+
+
+def make_data_files(roots):
+    data = []
+    
+    for root in roots:
+        if os.path.isdir(root):
+            for dirpath, dirnames, filenames in os.walk(root, True, None, False):
+                if filenames:
+                    data.append( (dirpath, [os.path.join(dirpath, f) for f in filenames]) )
+                if '__pycache__' in dirnames:
+                    dirnames.remove('__pycache__')
+        else:
+            data.append( ('', [root]) )
+    return data
+
+
+
+options = {
+  "dist_dir": "build/dist",
+  "includes": ["ctypes", "ctypes.wintypes", "decimal"], 
+  "excludes": [], 
+  "packages": []
+}
+
+data_files = make_data_files(['contrib', 'modules', 'simscript.ico'])
  
-setup(console=['tail.py'], windows=['simscript.py'],
-      options={ "py2exe": {"includes": ["ctypes", "ctypes.wintypes", "decimal"],
-                           "excludes": [],
-                           "packages": [] }}
-)
+distutils.core.setup(console=['tail.py'], windows=[ {'script':'simscript.py', 'icon_resources':[(1,"simscript.ico")]} ], options={'py2exe' :options}, data_files=data_files)

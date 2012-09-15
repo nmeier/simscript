80a021e887524eea43e387fcb5aef7ee55c89575
nils.meier <nils.meier@400e30b4-3047-0410-9b5d-373f348d38bd>
2012-09-15 03:11:36 +0000

    add zoom per pedal and hdg/crs increments via tuner
    git-svn-id: https://fscode.googlecode.com/svn/trunk@397 400e30b4-3047-0410-9b5d-373f348d38bd

--- scripts/myfalcon.py
+++ scripts/myfalcon.py
@@ -15,25 +15,28 @@
 #   Map Push Button into Button (Missile Uncage)
 #   Map Rotation into Axis (Missile Acquisition Sound Level)
 #
-import joysticks, phidgets, state, log, keyboard
+import joysticks, phidgets, state, log, keyboard, falcon
 
 vjoy = joysticks.get('vJoy Device')
 combatstick = joysticks.get("CH Combatstick USB")
+pedals = joysticks.get('CH Pro Pedals USB')
+
 shift = combatstick.getButton(3)
 
 # combatstick button for zoom axis toggle
 ZOOM_MIN = 1.0
 ZOOM_MAX = 0.35
 ZOOM_AXIS = 2
-axis = phidgets.Axis(phidgets.get(82141))
 zoomedout = state.get("zoomedout")
 if state.toggle("zoom-toggle", combatstick.getButton(2) and not shift) or zoomedout == None:
     log.info("zoom in" if zoomedout else "zoom out")
-    zoom = ZOOM_MAX if zoomedout else ZOOM_MIN
-    axis.setPosition(zoom)
-    state.set("zoomedout", not zoomedout)
-else:
-    zoom = axis.getPosition()
+    zoomedout = not zoomedout
+    state.set("zoomedout", zoomedout)
+
+if zoomedout:
+    zoom = ZOOM_MIN
+else: # pedals right for zoom past ZOOM_MAX
+    zoom = ZOOM_MAX - (pedals.getAxis(1,0.25)+1)/2
 vjoy.setAxis(ZOOM_AXIS, zoom)
 
 # combatstick shifted button for Freetrack reset
@@ -49,6 +52,27 @@ if ptt != optt and not shift:
     if ptt: keyboard.press(TEAMSPEAK_KEY)
     else: keyboard.release(TEAMSPEAK_KEY)
 
+# encoder 1 for hsi hdg/course inc/dec
+# Note: atm the only way to click hdg/crs up or down is to send multiple subsequent keyboard presses
+#       for each respective gauge modifier (-/+)1, (-/+)5 respectively
+#       I'm keeping track of the desired delta from encoder rotation and send only one keystroke per
+#       sync making sure that the call doesn't take too long and that (c)cw rotation stops fast if
+#       changing direction. Keys for 1/5 increment allow for two speeds to catch up.  
+tuner = phidgets.get(82141)
+dial = 'hdg' if not tuner.getInputState(0) else 'crs'
+delta = phidgets.delta(tuner, 45) + state.get(dial, 0)
+if delta!=0:
+    if delta>=5: inc = 5
+    elif delta>=1: inc = 1
+    elif delta>-5: inc = -1
+    else: inc = -5
+    delta -= inc
+    keys = { "hdg-1":"CTRL ALT V", "hdg+1":"CTRL ALT B", "hdg-5":"SHIFT ALT V", "hdg+5":"SHIFT ALT B" ,
+             "crs-1":"CTRL ALT N", "crs+1":"CTRL ALT M", "crs-5":"SHIFT ALT N", "crs+5":"SHIFT ALT M" }
+    keyboard.click( keys[ "%s%+d" % (dial,inc) ]  )
+state.set(dial, delta) # remaining
+    
+
 # encoder 2 for axis (one rotation) and button 
 ENCODER_2_AXIS = 1
 ENCODER_2_BUTTON = 1
@@ -56,8 +80,6 @@ ENCODER_2_BUTTON = 1
 encoder = phidgets.get(82081)
 vjoy.setAxis(ENCODER_2_AXIS, phidgets.Axis(encoder).getPosition())
 vjoy.setButton(ENCODER_2_BUTTON, encoder.getInputState(0))
-
-print(phidgets.delta(encoder))
 
 # saitek axis 2 into 2 buttons (AFBrakesOut/AFBrakesIn)
 AIRBREAK_OUT_BUTTON = 2

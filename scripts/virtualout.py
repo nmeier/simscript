7306e3e896f2a3c95f21bcdcb39de830961c3735
nils.meier <nils.meier@400e30b4-3047-0410-9b5d-373f348d38bd>
2012-10-04 03:30:09 +0000

    fix rounding problems in 2.7
    git-svn-id: https://fscode.googlecode.com/svn/trunk@417 400e30b4-3047-0410-9b5d-373f348d38bd

--- scripts/virtualout.py
+++ scripts/virtualout.py
@@ -10,10 +10,10 @@ loop = state.inc("loop")
 
 # rotate all axis
 for a in range(0,vjoy.numAxis()): 
-    vjoy.setAxis(a,math.sin(loop/10 + a * math.pi/8))
+    vjoy.setAxis(a,math.sin(loop/10.0 + a * math.pi/8.0))
 
 # go through buttons
 for b in range(0,vjoy.numButtons()):
     vjoy.setButton(b, False)
-vjoy.setButton(int(loop/10 % vjoy.numButtons()), True)    
+vjoy.setButton(int(loop/10.0 % vjoy.numButtons()), True)    
 
